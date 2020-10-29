85e670e9ed5ec5918bb0d07281df048bb33f9e64
Rodolfo Zamora <rodolfozamora999@gmail.com>
2020-10-29 12:41:24 -0700

    add more code and images

---
+++ filescode/tipo1.c
@@ -0,0 +1,25 @@
+/*Instituto Tecnologico de Tijuana
+
+Depto de Sistemas y Computación
+
+Ing. En Sistemas Computacionales
+
+Autor: Rodolfo Zamora Lopez @nickname RodolfoZamora999
+
+Repositorio: https://github.com/RodolfoZamora999/2.1_ARM32_Resumen (Respositorio privado)
+
+Fecha de revisión:  29/10/2020
+
+Objetivo del programa:
+Primer tipo de codigo de ejemplo del libro, se hace una muestra de codigo en C.*/
+
+
+int vi , vf , i ;
+for ( i= vi ; i <= vf ; i ++ ){
+/* Cuerpo del bucle */
+}
+i= vi ;
+while ( i <= vf ){
+/* Cuerpo del bucle */
+i ++;
+}
---
+++ filescode/tipo2.c
@@ -0,0 +1,23 @@
+/*Instituto Tecnologico de Tijuana
+
+Depto de Sistemas y Computación
+
+Ing. En Sistemas Computacionales
+
+Autor: Rodolfo Zamora Lopez @nickname RodolfoZamora999
+
+Repositorio: https://github.com/RodolfoZamora999/2.1_ARM32_Resumen (Respositorio privado)
+
+Fecha de revisión:  29/10/2020
+
+Objetivo del programa:
+Segundo tipo de muestra de codigo en C, se muestra un ejemplo de condicionales.*/
+
+
+int a , b ;
+if( a == b ){
+/* Có digo entonces */
+}
+else {
+/* Có digo sino */
+}
